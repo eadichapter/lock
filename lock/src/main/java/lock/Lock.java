137b22a889ad00ff69f50e110cd0d200b24b16f6
Stelios Omirou <omirous@interamerican.gr>
2021-12-30 16:44:58 +0200

    naive implementation - accepting just the right code

--- lock/src/main/java/lock/Lock.java
+++ lock/src/main/java/lock/Lock.java
@@ -1,9 +1,23 @@
 package lock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lock {
 	
+	private List<String> acceptedKeys;
+	
+	public Lock() {
+		acceptedKeys = new ArrayList<String>();
+	}
+	
 	public void accept(String key) {
-		
+		acceptedKeys.add(key);
+	}
+	
+	public boolean isLocked() {
+		return !acceptedKeys.equals(new ArrayList<String>(Arrays.asList("1", "3", "1")));
 	}
 
 }
--- lock/src/test/groovy/lock/LockSpec.groovy
+++ lock/src/test/groovy/lock/LockSpec.groovy
@@ -5,12 +5,40 @@ import spock.lang.Specification
 
 class LockSpec extends Specification {
 	
-	def "it accepts one number at a time" () {
+	def "it accepts one number at a time"() {
 		given: 'a new lock'
 		Lock lock = new Lock()
 		
 		expect: 'to accept a key'
 		lock.accept("1")
 	}
+	
+	def 'trying wrong combinations'() {
+		given: 'a new lock'
+		Lock lock = new Lock()
+		
+		when: 'to accept a key'
+		lock.accept("1")
+		lock.accept("2")
+		lock.accept("3")
+		lock.accept("4")
+		
+		then: 'the lock remains locked'
+		lock.isLocked() 
+	}
+	
+	def 'trying the right code'() {
+		given: 'a new lock'
+		Lock lock = new Lock()
+		
+		when: 'to accept a key'
+		lock.accept("1")
+		lock.accept("3")
+		lock.accept("1")
+		
+		then: 'the lock is unlocked'
+		!lock.isLocked()
+	}
+	
 	
 }
